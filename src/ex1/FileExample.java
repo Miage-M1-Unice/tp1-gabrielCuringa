cf36d467a261a5786dd3d7d0a338c9b914bec694
Gabriel Curinga <gabriel.curinga@outlook.fr>
2019-02-11 16:46:21 +0100

    fix classe interne q3

---
+++ src/ex1/FileExample.java
@@ -0,0 +1,80 @@
+package ex1;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class FileExample {
+
+    /**
+     * Test méthode list
+     * @param path
+     */
+    public void printDirectories(String path){
+
+        File file = new File(path);
+        for (String s: file.list()) {
+
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * Test méthode listFiles
+     * Affiche les répertoires et le contenu
+     * @param path
+     */
+    public void printDirectoriesAndFiles(String path){
+        File file = new File(path);
+        for (File f: file.listFiles()) {
+            if(f.isDirectory())
+                printDirectoriesAndFiles(f.getPath());
+            System.out.println(f);
+        }
+    }
+
+    /**
+     * Test méthode listFiles avec filtre
+     * @param path
+     * @param filter
+     */
+    public void printDirectoriesAndFilesFiltered(String path, FilenameFilter filter){
+        File file = new File(path);
+        for (File f: file.listFiles(filter)) {
+            if(f.isDirectory())
+                printDirectoriesAndFilesFiltered(f.getPath(), filter);
+            System.out.println(f);
+        }
+    }
+
+    /**
+     * Test méthode listFiles avec filtre
+     * UTILISATION CLASSE INTERNE
+     * @param path
+     * @param pattern
+     */
+    public void printDirectoriesAndFilesFilteredInternal(String path, String pattern){
+        File file = new File(path);
+        FilenameFilterInternal filter = new FilenameFilterInternal(pattern);
+        for (File f: file.listFiles(filter)) {
+            if(f.isDirectory())
+                printDirectoriesAndFilesFiltered(f.getPath(), filter);
+            System.out.println(f);
+        }
+    }
+
+    public class FilenameFilterInternal implements FilenameFilter {
+
+        private String pattern;
+
+        public FilenameFilterInternal(String pattern){
+            this.pattern = pattern;
+        }
+
+        @Override
+        public boolean accept(File dir, String name) {
+            File file = new File(dir.getPath()+"/"+name);
+            return name.endsWith(pattern) || file.isDirectory();
+        }
+    }
+
+}
--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -9,56 +9,32 @@ public class Main {
 
     public static void main(String[] args){
 
-        Main main = new Main();
+        FileExample fileExample = new FileExample();
 
-        main.printDirectories(DEFAULT_PATH);
+        fileExample.printDirectories(DEFAULT_PATH);
 
         System.out.println("------- FIN Q1 --------");
 
-        main.printDirectoriesAndFiles(DEFAULT_PATH);
+        fileExample.printDirectoriesAndFiles(DEFAULT_PATH);
 
         System.out.println("------- FIN Q2 --------");
 
-        FilenameFilter filter = new FilenameFilterExternal(".java");
-        main.printDirectoriesAndFilesFiltered(DEFAULT_PATH, filter);
+        // CLASSE EXTERNE
+        FilenameFilter externalFilter = new FilenameFilterExternal(".java");
+        //main.printDirectoriesAndFilesFiltered(DEFAULT_PATH, externalFilter);
+
+        // CLASSE ANONYME
+        /*main.printDirectoriesAndFilesFiltered(DEFAULT_PATH, new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                File file = new File(dir.getPath()+"/"+name);
+                return name.endsWith(".java") || file.isDirectory();
+            }
+        });*/
+
+        // CLASSE INTERNE NOMMÉE
+        fileExample.printDirectoriesAndFilesFilteredInternal(DEFAULT_PATH, ".java");
 
         System.out.println("------- FIN Q3 --------");
     }
-
-    /**
-     * Test méthode list
-     * @param path
-     */
-    private void printDirectories(String path){
-
-        File file = new File(path);
-        for (String s: file.list()) {
-
-            System.out.println(s);
-        }
-    }
-
-    /**
-     * Test méthode listFiles
-     * Affiche les répertoires et le contenu
-     * @param path
-     */
-    private void printDirectoriesAndFiles(String path){
-        File file = new File(path);
-        for (File f: file.listFiles()) {
-            if(f.isDirectory())
-                printDirectoriesAndFiles(f.getPath());
-            System.out.println(f);
-        }
-    }
-
-    private void printDirectoriesAndFilesFiltered(String path, FilenameFilter filter){
-        File file = new File(path);
-        for (File f: file.listFiles(filter)) {
-            if(f.isDirectory())
-                printDirectoriesAndFilesFiltered(f.getPath(), filter);
-            System.out.println(f);
-        }
-    }
-
 }
