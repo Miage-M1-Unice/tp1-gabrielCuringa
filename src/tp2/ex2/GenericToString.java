95d0f6a13c2273bb75b038d956dd0a36f84b045e
Gabriel Curinga <gabriel.curinga@outlook.fr>
2019-02-26 11:13:02 +0100

    amelioration de l'algo du toString

--- src/tp2/ex2/GenericToString.java
+++ src/tp2/ex2/GenericToString.java
@@ -2,44 +2,60 @@ package tp2.ex2;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GenericToString {
 
     public String toString(Object o, int profondeur) throws IllegalAccessException {
         Class aClass = o.getClass();
         String s = aClass.getName() + "[";
-        for (int i = 0; i < aClass.getDeclaredFields().length; i++) {
+        ArrayList<Field> fields = getAllFields(aClass);
+
+        for (int i = 0; i < fields.size(); i++) {
 
             if(profondeur <= 0)
                 return "";
 
-            Field field = aClass.getDeclaredFields()[i];
-            field.setAccessible(true); //on rend le champ accessible
+            fields.get(i).setAccessible(true); //on rend le champ accessible
 
-            if(!field.getType().isPrimitive()){
+            s += fields.get(i).getName() +"="; //name
 
-                if(field.getType().isArray()){
-                    s += field.getName();
-                    s += "={";
-                    for (int j = 0; j < Array.getLength(field.get(o)); j++) {
-
-                        s += Array.get(field.get(o), j);
-                        if(j != Array.getLength(field.get(o))-1)
-                            s += ",";
-                    }
-                    s += "}";
-                }else
-                    s += toString(field.get(o), profondeur-1);
-
-            }else{
-                s += field.getName() +"="; //name
-                s += field.get(o);//value
+            if(fields.get(i).getType().isPrimitive()){
+                s += fields.get(i).get(o);//value
                 //fin de champ
                 s += "; ";
+
+
+            }else if(fields.get(i).getType().isArray()){
+
+                s += fields.get(i).getName();
+                s += "={";
+                for (int j = 0; j < Array.getLength(fields.get(i).get(o)); j++) {
+
+                    s += Array.get(fields.get(i).get(o), j);
+                    if(j != Array.getLength(fields.get(i).get(o))-1)
+                        s += ",";
+                }
+                s += "}; ";
+            }else{
+                s += toString(fields.get(i).get(o), profondeur-1)+"\n";
+
             }
 
         }
 
         return s+"]";
+    }
+
+    private ArrayList<Field> getAllFields(Class aClass){
+
+        ArrayList<Field> fields = new ArrayList<>(Arrays.asList(aClass.getFields()));
+
+        for (Field field : aClass.getDeclaredFields()) {
+            if(!fields.contains(field))
+                fields.add(field);
+        }
+        return fields;
     }
 }
